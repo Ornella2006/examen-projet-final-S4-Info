f45df65a9d0b58b1fbb0a1d3bfa708e645c55ca6
OrnellaMarion <168301329+Ornella2006@users.noreply.github.com>
2025-07-07 15:10:45 +0300

    Remplacement complet du projet initial par un nouveau projet

--- ws/index.php
+++ ws/index.php
@@ -1,16 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 require 'db.php';
-use App\Controllers\ClientController;
-
-
-Flight::route('GET /clients', [ClientController::class, 'index']);
-Flight::route('GET /clients/@id', [ClientController::class, 'show']);
-Flight::route('POST /clients', [ClientController::class, 'store']);
-Flight::route('PUT /clients/@id', [ClientController::class, 'update']);
-Flight::route('DELETE /clients/@id', [ClientController::class, 'destroy']);
-
-// Route pour afficher la vue HTML des clients
-Flight::route('GET /clients-view', [ClientController::class, 'indexView']);
+require 'routes/etudiant_routes.php';
 
 Flight::start();
